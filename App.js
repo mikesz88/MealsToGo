e8f329041b2a3c50793d7a7e6f7d9c229314b699
michael <michaelsanchez8815@gmail.com>
2022-08-04 10:27:09 -0500

    Create Searchbar and eslint/prettier

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { Searchbar } from 'react-native-paper';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const onChangeSearch = (query) => setSearchQuery(query);
+
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <View style={styles.searchContainer}>
-          <Text>Search</Text>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
         </View>
-        <View style={styles.listContainer}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style='auto' />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
@@ -22,13 +38,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
-  searchContainer: {
-    backgroundColor: 'green',
+  search: {
+    backgroundColor: '#fff',
     padding: 16,
   },
-  listContainer: {
+  list: {
     flex: 1,
     backgroundColor: 'blue',
     padding: 16,
-  }
+  },
 });
