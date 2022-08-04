84eb7d9a51031d815d676f42b97d830da1ff3277
michael <michaelsanchez8815@gmail.com>
2022-08-04 14:20:40 -0500

    Finish styling restaurant card

--- App.js
+++ App.js
@@ -3,8 +3,25 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { theme } from './src/infrastructure/theme';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [LatoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!OswaldLoaded || !LatoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
